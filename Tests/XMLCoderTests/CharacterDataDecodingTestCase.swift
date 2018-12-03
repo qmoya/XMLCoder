c0d388341068fdc6a2a8d37fd0c320c9c4c7c82e
Quico Moya <me@qmoya.com>
2018-12-03 14:00:52 +0100

    Parse chardata

--- Tests/XMLCoderTests/CharacterDataDecodingTestCase.swift
+++ Tests/XMLCoderTests/CharacterDataDecodingTestCase.swift
@@ -7,7 +7,47 @@ class CharacterDataDecodingTestCase: DecodingTestCase {
         super.setUp()
         decoder.characterDataToken = "#text"
     }
-    
+
+    func testItDecodesValuesCodedAsElements() {
+        // given
+        struct Element: Decodable, Equatable {
+            let key: String?
+        }
+        let xml = """
+        <element><key>My value</key></element>
+        """
+
+        do {
+            // when
+            let el: Element = try decode(xml)
+
+            // then
+            XCTAssertEqual(el, Element(key: "My value"))
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+
+    func testItDecodesValuesCodedAsAttributes() {
+        // given
+        struct Element: Decodable, Equatable {
+            let key: String?
+        }
+        let xml = """
+        <element key="My value"></element>
+        """
+
+        do {
+            // when
+            let el: Element = try decode(xml)
+
+            // then
+            XCTAssertEqual(el, Element(key: "My value"))
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+
     func testItDecodesCharData() {
         // given
         struct Attribute: Decodable, Equatable {
@@ -15,56 +55,66 @@ class CharacterDataDecodingTestCase: DecodingTestCase {
                 case name
                 case value = "#text"
             }
-            
+
             let name: String
             let value: String
         }
         let xml = """
         <attribute name="Xpos">0</attribute>
         """
-        
-        // when
-        let attr: Attribute = try! decode(xml)
-        
-        // then
-        XCTAssertEqual(attr, Attribute(name: "Xpos", value: "0"))
+
+        do {
+            // when
+            let attr: Attribute = try decode(xml)
+
+            // then
+            XCTAssertEqual(attr, Attribute(name: "Xpos", value: "0"))
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
     }
 
-	func testItDecodesCharDataWhenNested() {
-		// given
-		struct Item: Decodable, Equatable {
-			enum CodingKeys: String, CodingKey {
-				case id = "ID"
-				case creator = "Creator"
-				case children = "attribute"
-			}
+    func testItDecodesCharDataWhenNested() {
+        // given
+        struct Item: Decodable, Equatable {
+            enum CodingKeys: String, CodingKey {
+                case id = "ID"
+                case creator = "Creator"
+                case children = "attribute"
+            }
 
-			let id: String
-			let creator: String
-			let children: [Attribute]?
-		}
+            let id: String
+            let creator: String
+            let children: [Attribute]?
+        }
 
-		struct Attribute: Decodable, Equatable {
-			enum CodingKeys: String, CodingKey {
-				case name
-				case value = "#text"
-			}
+        struct Attribute: Decodable, Equatable {
+            enum CodingKeys: String, CodingKey {
+                case name
+                case value = "#text"
+            }
 
-			let name: String
-			let value: String?
-		}
+            let name: String
+            let value: String?
+        }
 
-		let xml = """
-			<item ID="1542637462" Creator="Francisco Moya">
-			<attribute name="Xpos">0</attribute>
-			</item>
-			"""
+        let xml = """
+        <item ID="1542637462" Creator="Francisco Moya">
+        <attribute name="Xpos">0</attribute>
+        </item>
+        """
 
-		// when
-		let result: Item = try! decode(xml)
+        // when
+        do {
+            let result: Item = try decode(xml)
 
-		// then
-		let expected = Item(id: "1542637462", creator: "Francisco Moya", children: nil)
-		XCTAssertEqual(result, expected)
-	}
+            // then
+            let expected = Item(id: "1542637462", creator: "Francisco Moya", children: [
+                Attribute(name: "Xpos", value: "0"),
+            ])
+            XCTAssertEqual(result, expected)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
 }
--- Tests/XMLCoderTests/DecodingTestCase.swift
+++ Tests/XMLCoderTests/DecodingTestCase.swift
@@ -4,18 +4,18 @@ import XMLCoder
 
 class DecodingTestCase: XCTestCase {
     var decoder: XMLDecoder!
-    
+
     func decode<T>(_ string: String) throws -> T where T: Decodable {
         let data = string.data(using: .utf8)!
         let result: T = try decoder.decode(T.self, from: data)
         return result
     }
-    
+
     override func setUp() {
         super.setUp()
         decoder = XMLDecoder()
     }
-    
+
     override func tearDown() {
         decoder = nil
         super.tearDown()
